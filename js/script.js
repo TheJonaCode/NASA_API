fd04a6fe2620d5b8f79356702adb523229678626
TheJonaCode <mcjhona069@gmail.com>
2022-12-14 19:22:33 -0600

    showing info

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
+/*Get DOM elements*/
+const apod__title = document.querySelector('.title__apod');
+const apod__description = document.querySelector('.desc__container');
+const apod__image = document.querySelector('.apod__img');
 
+$(document).ready(function() {
     //Starting AOS Library
     AOS.init();
-
-    //TODO: Get API
 });
 
 //Get API
@@ -11,6 +13,14 @@ async function fetchInfo() {
     const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=oCpQ0U9g3fPD67rGbDl2TrhpLT6L4ieahPOZHZmh');
     const data = await res.json();
     console.log(data);
+
+    //Title
+    apod__title.innerHTML = '"' + data.title + '"';
+    //IMG
+    apod__image.src = data.url;
+    //Description
+    apod__description.innerHTML = data.explanation;
 }
+
 
 fetchInfo();
